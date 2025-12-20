ea45013d7f975fa3d37060ec8574e3bc35bbcf67
Wm Salt Hale <salt@sal.td>
2025-12-19 17:46:41 -0800

    fix build, add build all

--- pdf-generator/wyc_md-to-pdf.sh
+++ pdf-generator/wyc_md-to-pdf.sh
@@ -2,9 +2,9 @@
 set -Eeuo pipefail
 
 npx puppeteer browsers install chrome
-npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_Constitution.md
-npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_By-Laws.md
-npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_Policies.md
-npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_Ithaka_Appendix.md
+npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json ../WYC_Constitution.md
+npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json ../WYC_By-Laws.md
+npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json ../WYC_Policies.md
+npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json ../WYC_Ithaka_Appendix.md
 
 exit 0

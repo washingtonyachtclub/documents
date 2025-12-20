3a633b3ea901f3cd7db4f0f894aab6916822eb15
Wm Salt Hale <salt@sal.td>
2025-12-19 16:54:33 -0800

    fix pdf rendering

--- wyc_md-to-pdf.sh
+++ wyc_md-to-pdf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
+npx puppeteer browsers install chrome
 npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_Constitution.md
 npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_By-Laws.md
 npx md-to-pdf --config-file ./wyc_md-to-pdf_config.json WYC_Policies.md
